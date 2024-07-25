400f3493ce59fa39d239ee5e459c17ba881d8b51
Audrey Luquet <audrey.luquet@tutanota.com>
2024-07-25 17:10:53 +0200

    tentative add plusieurs todolist

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,27 +26,30 @@ if (titleTodo) {
 }
 
 addList.addEventListener("click", () => {
+   /* for (let i; tabList >= 0; i++) { */
    createList()
+   /* console.log("index " + tabList.id)
+   } */
 })
 
 /** Create Todolist **/
 const toDoList = document.getElementById("todoList")
+toDoList.innerHTML = ""
 
 let tabList = []
-// boucle où et comment ??
 
 function createList() {
    toDoList.innerHTML = `
-    <article id="${tabList.id}">
-        <i class="fa-solid fa-trash-can"></i>
-        <h3>${titleTodo.value}</h3>
-        <form>
+      <article id="${tabList.id}">
+         <i class="fa-solid fa-trash-can"></i>
+         <h3>${titleTodo.value}</h3>
+         <form>
             <input type="text" placeholder="Nom de la tâche" required>
             <button id="addtask">Ajouter</button>
-        </form>
-        <ul>
+         </form>
+         <ul>
             <!-- vide de base -->
-        </ul>
-    </article>
-  `
+         </ul>
+      </article>
+   `
 }
