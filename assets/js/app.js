41c2b424678a0988cb7fa4b5be5bfcf2de8eda4d
Audrey Luquet <audrey.luquet@tutanota.com>
2024-07-29 19:49:16 +0200

    mise en place close modal après appel createList()

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
 /** Open / Close Modal **/
-const dialog = document.querySelector("dialog")
+const dialog = document.getElementById("modal")
 const showDialog = document.getElementById("openModal")
 const closeDialog = document.getElementById("close")
 
 showDialog.addEventListener("click", () => {
    dialog.showModal()
+   dialog.style.display = "flex"
 })
 closeDialog.addEventListener("click", () => {
    dialog.close()
+   dialog.style.display = "none"
+})
+dialog.addEventListener("click", (event) => {
+   if (event.target === modal) {
+      dialog.close()
+      dialog.style.display = "none"
+   }
 })
 
 /** Vérif Form **/
@@ -30,11 +38,66 @@ const toDoList = document.getElementById("todoList")
 
 function createList() {
    const list = document.createElement("article")
-   list.innerHTML = `<i onclick="this.parentElement.remove()" class="fa-solid fa-trash-can"></i>`
+
+   const deleteIcon = document.createElement("i")
+   deleteIcon.className = "fa-solid fa-trash-can"
+   deleteIcon.onclick = () => list.remove()
+   list.appendChild(deleteIcon)
+
    const titleList = document.createElement("h3")
-   const txtTitleList = document.getElementById("titleTodo").value.trim() // enlève espace dbt et fin
+   const txtTitleList = document.getElementById("titleTodo").value.trim() // trim => enlève espace début et fin
    const title = document.createTextNode(txtTitleList)
    titleList.appendChild(title)
    list.appendChild(titleList)
+
+   const form = document.createElement("form")
+   form.onsubmit = function (event) {
+      event.preventDefault()
+      createTask(form)
+   }
+
+   const inputTask = document.createElement("input")
+   inputTask.id = "nameTask"
+   inputTask.type = "text"
+   inputTask.required = true
+   form.appendChild(inputTask)
+
+   const button = document.createElement("button")
+   button.textContent = "Ajouter"
+   button.type = "submit"
+   form.appendChild(button)
+
+   list.appendChild(form)
+
+   const ulTask = document.createElement("ul")
+   list.appendChild(ulTask)
+
    toDoList.appendChild(list)
+
+   dialog.close()
+   dialog.style.display = "none"
+}
+
+/** Create Task with Delete **/
+function createTask(form) {
+   const taskList = form.parentElement.querySelector("ul") // Cherche ul dans article
+   const task = document.createElement("li")
+
+   const check = document.createElement("input")
+   check.type = "checkbox"
+   task.appendChild(check)
+
+   const nameTask = document.createElement("p")
+   const txtNametask = form.querySelector("#nameTask").value.trim() // Recup valeur via form
+   const name = document.createTextNode(txtNametask)
+   nameTask.appendChild(name)
+   task.appendChild(nameTask)
+
+   const deleteTaskIcon = document.createElement("i")
+   deleteTaskIcon.className = "fa-solid fa-trash-can"
+   deleteTaskIcon.onclick = () => task.remove()
+   task.appendChild(deleteTaskIcon)
+
+   taskList.appendChild(task)
+   form.reset()
 }
